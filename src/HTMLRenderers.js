8422915a3277ac60e6ce3c2632d3f47277767f02
Kelly Packer <kelly@kellypacker.com>
2019-03-01 10:08:54 -0700

    Adjust down margin of uls

--- src/HTMLRenderers.js
+++ src/HTMLRenderers.js
@@ -98,7 +98,7 @@ export function ul (htmlAttribs, children, convertedCSSStyles, passProps = {}) {
             }
         }
         return (
-            <View key={`list-${nodeIndex}-${index}-${key}`} style={{ flexDirection: 'row', marginBottom: 10 }}>
+            <View key={`list-${nodeIndex}-${index}-${key}`} style={{ flexDirection: 'row', marginBottom: 5 }}>
                 { prefix }
                 <View style={{ flex: 1 }}>{ child }</View>
             </View>
